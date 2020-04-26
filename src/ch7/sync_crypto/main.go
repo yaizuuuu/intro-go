daf6408eda9c1284818697d8326826f839a4fdef
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-04-26 23:54:54 +0900

    chapter7 sync写経中

---
+++ src/ch7/sync_crypto/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		go func() {
+			for i := 0; i < 1000; i++ {
+				UpdateAndPrint(i)
+			}
+		}()
+	}
+	for {
+
+	}
+}
+
+var st struct{A, B, C int}
+
+func UpdateAndPrint(n int)  {
+	st.A = n
+	time.Sleep(time.Microsecond)
+	st.B = n
+	time.Sleep(time.Microsecond)
+	st.C = n
+	time.Sleep(time.Microsecond)
+
+	fmt.Println(st.A, st.B, st.C)
+}
