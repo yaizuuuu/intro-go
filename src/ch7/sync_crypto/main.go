a533d61784122fa29f853cc85b1ba1ad7b0e3ccb
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-04-27 00:42:52 +0900

    chapter7 sync,cryptoを写経

--- src/ch7/sync_crypto/main.go
+++ src/ch7/sync_crypto/main.go
@@ -1,32 +1,80 @@
 package main
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
 	"fmt"
-	"time"
+	"io"
+	"sync"
 )
 
+//func main() {
+//	mutex = new(sync.Mutex)
+//
+//	for i := 0; i < 5; i++ {
+//		go func() {
+//			for i := 0; i < 10; i++ {
+//				UpdateAndPrint(i)
+//			}
+//		}()
+//	}
+//	for {}
+//}
+//
+//var st struct{A, B, C int}
+//
+//// ミューテックスを保持する
+//var mutex *sync.Mutex
+//
+//func UpdateAndPrint(n int)  {
+//	// ロック
+//	// これにより処理を同期化できる
+//	mutex.Lock()
+//
+//	st.A = n
+//	time.Sleep(time.Microsecond)
+//	st.B = n
+//	time.Sleep(time.Microsecond)
+//	st.C = n
+//	time.Sleep(time.Microsecond)
+//
+//	fmt.Println(st.A, st.B, st.C)
+//
+//	// アンロック
+//	mutex.Unlock()
+//}
+
 func main() {
-	for i := 0; i < 5; i++ {
-		go func() {
-			for i := 0; i < 1000; i++ {
-				UpdateAndPrint(i)
-			}
-		}()
-	}
-	for {
+	wg := new(sync.WaitGroup)
+	wg.Add(3)
 
-	}
-}
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("1st Goroutine")
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("2nd Goroutine")
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("3rd Goroutine")
+		}
+		wg.Done()
+	}()
 
-var st struct{A, B, C int}
+	// WaitGroupのすべての終了を検知するまで待機
+	wg.Wait()
 
-func UpdateAndPrint(n int)  {
-	st.A = n
-	time.Sleep(time.Microsecond)
-	st.B = n
-	time.Sleep(time.Microsecond)
-	st.C = n
-	time.Sleep(time.Microsecond)
+	h := md5.New()
+	io.WriteString(h, "ABCDE")
+	fmt.Println(h.Sum(nil))
 
-	fmt.Println(st.A, st.B, st.C)
+	s1 := sha1.New()
+	io.WriteString(s1, "ABCDE")
+	fmt.Printf("%x\n", s1.Sum(nil))
 }
